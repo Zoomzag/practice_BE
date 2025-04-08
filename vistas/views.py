4af07053a3f9bd1f6df23be6a08f0e73f4607e37
Jesús Daniel Loya Millán <118396894+Zoomzag@users.noreply.github.com>
2025-04-07 18:11:44 -0600

    comit
    actualización 1

---
+++ vistas/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def inicio(request):
+    return render(request, 'Practica_class/index.html')
+
+def about(request):
+    return render(request, 'Practica/about.html')
+
+def about(request):
+    return render(request, 'Practica/contact.html')
+
+def about(request):
+    return render(request, 'Practica/games.html')
+
+
